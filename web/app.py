65e6f905e2b98a0bacd0541cc746ae497a1a8b6e
sharper <softliumin@gmail.com>
2016-10-14 10:36:14 +0800

    add code

--- web/app.py
+++ web/app.py
@@ -2,13 +2,34 @@
 #coding=utf-8
 from flask import Flask,render_template
 from flask import request
+import MySQLdb
+
 
 app = Flask(__name__)
 
 @app.route('/',methods=['GET','POST'])
 def home():
     #return '<h1>1111</h1>'
-    return render_template('/page/all.html')
+    # 在这里进行数据的查询和放入模板
+    conn = MySQLdb.connect(
+        host='101.200.145.47',
+        port=3306,
+        user='root',
+        passwd='liumin110',
+        db='liumin2',
+    )
+
+
+    cur=conn.cursor()
+    cur.execute("select * from sss")
+    result = cur.fetchall()
+
+    return render_template('/page/all.html',name='haha',pp=result)
+# cur.execute("insert into sss values(111,'huhu')")
+
+
+
+
 
 
 @app.route('/login',methods=['GET'])
@@ -28,4 +49,4 @@ def login2():
     return '<h1>Bad Login Info</h1>'
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run()#debug=True
